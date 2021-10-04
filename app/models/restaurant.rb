44beef3479d8287e0bf4e3c297f049c599ec179f
Gergo Karcagi <karcagigergo@gmail.com>
2021-10-04 18:04:04 -0300

    CRUD actions done with API requests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :name, presence :true, uniqueness :true
+  validates :name, presence: true, uniqueness: true
 end
